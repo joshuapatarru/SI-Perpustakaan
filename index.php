c56917569d571ed22edda8f5152ca81f524ea262
joshuapatarru <152599724+joshuapatarru@users.noreply.github.com>
2024-04-16 19:12:27 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>perpustakaan undip</title>
+<link rel="stylesheet" href="s">
+<style>
+  body {
+    background-color: #1E90FF; /* Warna biru muda */
+  }
+</style>
+</head>
+<body>
+<center>
+
+<form action="login_proses.php" method="post">
+  <h2>Perpustakaan Universitas Brawijaya</h2>
+  <hr>
+  <div class="imgcontainer">
+    <img src="https://kompaspedia.kompas.id/wp-content/uploads/2020/07/logo_Universitas-Brawijaya-thumb.png" alt="Avatar" class="avatar">
+  </div>
+
+  <div class="container">
+    <br>
+    <input type="text" placeholder="Enter Username" name="name" required><br>
+
+    <br>
+    <input type="password" placeholder="Enter Password" name="pw" required><br>
+    <br>
+    <button type="submit">Login</button><br>
+</center>
+</form>
+
+</body>
+</html>
